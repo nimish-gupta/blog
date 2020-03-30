1410921ea22befb712cf854cf5daaf22b532599a
Nimish <nimishgupta18396@gmail.com>
2020-03-30 18:03:11 +0530

    fix: corrected condition for link display

--- src/components/Sidebar/Menu/Menu.js
+++ src/components/Sidebar/Menu/Menu.js
@@ -11,32 +11,31 @@ type Props = {
 	}[],
 };
 
-const Menu = ({ menu }: Props) =>
-	console.log(menu) || (
-		<nav className={styles['menu']}>
-			<ul className={styles['menu__list']}>
-				{menu.map((item) => (
-					<li className={styles['menu__list-item']} key={item.path}>
-						{item.path !== undefined ? (
-							<Link
-								to={item.path}
-								className={styles['menu__list-item-link']}
-								activeClassName={styles['menu__list-item-link--active']}>
-								{item.label}
-							</Link>
-						) : (
-							<a
-								className={styles['menu__list-item-link']}
-								target="_blank"
-								rel="noreferer noopener"
-								href={item.href}>
-								{item.label}
-							</a>
-						)}
-					</li>
-				))}
-			</ul>
-		</nav>
-	);
+const Menu = ({ menu }: Props) => (
+	<nav className={styles['menu']}>
+		<ul className={styles['menu__list']}>
+			{menu.map((item) => (
+				<li className={styles['menu__list-item']} key={item.path}>
+					{item.path !== undefined && item.path !== null ? (
+						<Link
+							to={item.path}
+							className={styles['menu__list-item-link']}
+							activeClassName={styles['menu__list-item-link--active']}>
+							{item.label}
+						</Link>
+					) : (
+						<a
+							className={styles['menu__list-item-link']}
+							target="_blank"
+							rel="noreferer noopener"
+							href={item.href || ''}>
+							{item.label}
+						</a>
+					)}
+				</li>
+			))}
+		</ul>
+	</nav>
+);
 
 export default Menu;
