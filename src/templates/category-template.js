feedfcb8ec3e104d50d3197cacb7d28ca714b983
Nimish <nimishgupta18396@gmail.com>
2020-04-01 00:47:01 +0530

    fix: added the reading time in categories and tag template

--- src/templates/category-template.js
+++ src/templates/category-template.js
@@ -10,65 +10,77 @@ import { useSiteMetadata } from '../hooks';
 import type { PageContext, AllMarkdownRemark } from '../types';
 
 type Props = {
-  data: AllMarkdownRemark,
-  pageContext: PageContext
+	data: AllMarkdownRemark,
+	pageContext: PageContext,
 };
 
 const CategoryTemplate = ({ data, pageContext }: Props) => {
-  const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
+	const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
 
-  const {
-    category,
-    currentPage,
-    prevPagePath,
-    nextPagePath,
-    hasPrevPage,
-    hasNextPage,
-  } = pageContext;
+	const {
+		category,
+		currentPage,
+		prevPagePath,
+		nextPagePath,
+		hasPrevPage,
+		hasNextPage,
+	} = pageContext;
 
-  const { edges } = data.allMarkdownRemark;
-  const pageTitle = currentPage > 0 ? `${category} - Page ${currentPage} - ${siteTitle}` : `${category} - ${siteTitle}`;
+	const { edges } = data.allMarkdownRemark;
+	const pageTitle =
+		currentPage > 0
+			? `${category} - Page ${currentPage} - ${siteTitle}`
+			: `${category} - ${siteTitle}`;
 
-  return (
-    <Layout title={pageTitle} description={siteSubtitle}>
-      <Sidebar />
-      <Page title={category}>
-        <Feed edges={edges} />
-        <Pagination
-          prevPagePath={prevPagePath}
-          nextPagePath={nextPagePath}
-          hasPrevPage={hasPrevPage}
-          hasNextPage={hasNextPage}
-        />
-      </Page>
-    </Layout>
-  );
+	return (
+		<Layout title={pageTitle} description={siteSubtitle}>
+			<Sidebar />
+			<Page title={category}>
+				<Feed edges={edges} />
+				<Pagination
+					prevPagePath={prevPagePath}
+					nextPagePath={nextPagePath}
+					hasPrevPage={hasPrevPage}
+					hasNextPage={hasNextPage}
+				/>
+			</Page>
+		</Layout>
+	);
 };
 
 export const query = graphql`
-  query CategoryPage($category: String, $postsLimit: Int!, $postsOffset: Int!) {
-    allMarkdownRemark(
-        limit: $postsLimit,
-        skip: $postsOffset,
-        filter: { frontmatter: { category: { eq: $category }, template: { eq: "post" }, draft: { ne: true } } },
-        sort: { order: DESC, fields: [frontmatter___date] }
-      ){
-      edges {
-        node {
-          fields {
-            categorySlug
-            slug
-          }
-          frontmatter {
-            date
-            description
-            category
-            title
-          }
-        }
-      }
-    }
-  }
+	query CategoryPage($category: String, $postsLimit: Int!, $postsOffset: Int!) {
+		allMarkdownRemark(
+			limit: $postsLimit
+			skip: $postsOffset
+			filter: {
+				frontmatter: {
+					category: { eq: $category }
+					template: { eq: "post" }
+					draft: { ne: true }
+				}
+			}
+			sort: { order: DESC, fields: [frontmatter___date] }
+		) {
+			edges {
+				node {
+					fields {
+						categorySlug
+						slug
+						readingTime {
+							text
+						}
+					}
+					frontmatter {
+						date
+						description
+						category
+						title
+					}
+				}
+			}
+		}
+	}
 `;
 
 export default CategoryTemplate;
--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -10,71 +10,83 @@ import { useSiteMetadata } from '../hooks';
 import type { AllMarkdownRemark, PageContext } from '../types';
 
 type Props = {
-  data: AllMarkdownRemark,
-  pageContext: PageContext
+	data: AllMarkdownRemark,
+	pageContext: PageContext,
 };
 
 const TagTemplate = ({ data, pageContext }: Props) => {
-  const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
+	const { title: siteTitle, subtitle: siteSubtitle } = useSiteMetadata();
 
-  const {
-    tag,
-    currentPage,
-    prevPagePath,
-    nextPagePath,
-    hasPrevPage,
-    hasNextPage
-  } = pageContext;
+	const {
+		tag,
+		currentPage,
+		prevPagePath,
+		nextPagePath,
+		hasPrevPage,
+		hasNextPage,
+	} = pageContext;
 
-  const { edges } = data.allMarkdownRemark;
-  const pageTitle = currentPage > 0 ? `All Posts tagged as "${tag}" - Page ${currentPage} - ${siteTitle}` : `All Posts tagged as "${tag}" - ${siteTitle}`;
+	const { edges } = data.allMarkdownRemark;
+	const pageTitle =
+		currentPage > 0
+			? `All Posts tagged as "${tag}" - Page ${currentPage} - ${siteTitle}`
+			: `All Posts tagged as "${tag}" - ${siteTitle}`;
 
-  return (
-    <Layout title={pageTitle} description={siteSubtitle}>
-      <Sidebar />
-      <Page title={tag}>
-        <Feed edges={edges} />
-        <Pagination
-          prevPagePath={prevPagePath}
-          nextPagePath={nextPagePath}
-          hasPrevPage={hasPrevPage}
-          hasNextPage={hasNextPage}
-        />
-      </Page>
-    </Layout>
-  );
+	return (
+		<Layout title={pageTitle} description={siteSubtitle}>
+			<Sidebar />
+			<Page title={tag}>
+				<Feed edges={edges} />
+				<Pagination
+					prevPagePath={prevPagePath}
+					nextPagePath={nextPagePath}
+					hasPrevPage={hasPrevPage}
+					hasNextPage={hasNextPage}
+				/>
+			</Page>
+		</Layout>
+	);
 };
 
 export const query = graphql`
-  query TagPage($tag: String, $postsLimit: Int!, $postsOffset: Int!) {
-    site {
-      siteMetadata {
-        title
-        subtitle
-      }
-    }
-    allMarkdownRemark(
-        limit: $postsLimit,
-        skip: $postsOffset,
-        filter: { frontmatter: { tags: { in: [$tag] }, template: { eq: "post" }, draft: { ne: true } } },
-        sort: { order: DESC, fields: [frontmatter___date] }
-      ){
-      edges {
-        node {
-          fields {
-            slug
-            categorySlug
-          }
-          frontmatter {
-            title
-            date
-            category
-            description
-          }
-        }
-      }
-    }
-  }
+	query TagPage($tag: String, $postsLimit: Int!, $postsOffset: Int!) {
+		site {
+			siteMetadata {
+				title
+				subtitle
+			}
+		}
+		allMarkdownRemark(
+			limit: $postsLimit
+			skip: $postsOffset
+			filter: {
+				frontmatter: {
+					tags: { in: [$tag] }
+					template: { eq: "post" }
+					draft: { ne: true }
+				}
+			}
+			sort: { order: DESC, fields: [frontmatter___date] }
+		) {
+			edges {
+				node {
+					fields {
+						slug
+						categorySlug
+						readingTime {
+							text
+						}
+					}
+					frontmatter {
+						title
+						date
+						category
+						description
+					}
+				}
+			}
+		}
+	}
 `;
 
 export default TagTemplate;
