ffeb2114c13b6e8150b3f7e952bce896d5fb771e
Nimish <nimishgupta18396@gmail.com>
2020-04-23 23:22:55 +0530

    fix: replaced Icon scss

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,20 +1,20 @@
 // @flow strict
 import React from 'react';
-import styles from './Icon.module.scss';
+import StyledIcon from './Icon.style';
 
 type Props = {
-  name: string,
-  icon: {
-    viewBox?: string,
-    path?: string
-  }
+	name: string,
+	icon: {
+		viewBox?: string,
+		path?: string,
+	},
 };
 
 const Icon = ({ name, icon }: Props) => (
-  <svg className={styles['icon']} viewBox={icon.viewBox}>
-    <title>{name}</title>
-    <path d={icon.path} />
-  </svg>
+	<StyledIcon viewBox={icon.viewBox}>
+		<title>{name}</title>
+		<path d={icon.path} />
+	</StyledIcon>
 );
 
 export default Icon;
