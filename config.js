44be3f0002fa6797409f417639832e7ba809bf95
Nimish <nimishgupta18396@gmail.com>
2020-08-19 23:32:05 +0530

    fix: remove the portfolio link

--- config.js
+++ config.js
@@ -1,58 +1,54 @@
 'use strict';
 
 module.exports = {
-	url: 'https://nimish-dev.com',
-	adminUrl: 'https://blissful-raman-6fcfa5.netlify.com/admin',
-	pathPrefix: '/',
-	title: 'Engineer Diary',
-	subtitle: 'A Pragmatic engineer learning to program deliberately.',
-	copyright: '© All rights reserved.',
-	disqusShortname: 'nimish-gupta',
-	postsPerPage: 4,
-	googleAnalyticsId: 'UA-162360089-1',
-	useKatex: false,
-	menu: [
-		{
-			label: 'Articles',
-			path: '/',
-		},
-		// {
-		// 	label: 'About me',
-		// 	path: '/pages/about',
-		// },
-		// {
-		// 	label: 'Contact me',
-		// 	path: '/pages/contacts',
-		// },
-		{
-			label: 'Portfolio',
-			href: 'https://portfolio.nimish-gupta.dev/',
-		},
-		{
-			label: 'Stalk Me',
-			href: 'https://gitstalk.netlify.app/nimish-gupta/',
-		},
-	],
-	author: {
-		name: 'Nimish Gupta',
-		photo: '/photo.jpeg',
-		bio: 'A Pragmatic engineer learning to program deliberately.',
-		contacts: {
-			email: 'nimishgupta18396@gmail.com',
-			facebook: 'nimishrocks',
-			telegram: 'nimish_gupta',
-			twitter: 'nimishrocks2',
-			github: 'nimish-gupta',
-			rss: '',
-			vkontakte: '',
-			linkedin: 'nimish-gupta',
-			instagram: '',
-			line: '',
-			gitlab: '',
-			weibo: '',
-			codepen: '',
-			youtube: '',
-			soundcloud: '',
-		},
-	},
+    url: 'https://nimish-dev.com',
+    adminUrl: 'https://blissful-raman-6fcfa5.netlify.com/admin',
+    pathPrefix: '/',
+    title: 'Engineer Diary',
+    subtitle: 'A Pragmatic engineer learning to program deliberately.',
+    copyright: '© All rights reserved.',
+    disqusShortname: 'nimish-gupta',
+    postsPerPage: 4,
+    googleAnalyticsId: 'UA-162360089-1',
+    useKatex: false,
+    menu: [
+        {
+            label: 'Articles',
+            path: '/'
+        },
+        // {
+        // 	label: 'About me',
+        // 	path: '/pages/about',
+        // },
+        // {
+        // 	label: 'Contact me',
+        // 	path: '/pages/contacts',
+        // },
+        {
+            label: 'Stalk Me',
+            href: 'https://gitstalk.netlify.app/nimish-gupta/'
+        }
+    ],
+    author: {
+        name: 'Nimish Gupta',
+        photo: '/photo.jpeg',
+        bio: 'A Pragmatic engineer learning to program deliberately.',
+        contacts: {
+            email: 'nimishgupta18396@gmail.com',
+            facebook: 'nimishrocks',
+            telegram: 'nimish_gupta',
+            twitter: 'nimishrocks2',
+            github: 'nimish-gupta',
+            rss: '',
+            vkontakte: '',
+            linkedin: 'nimish-gupta',
+            instagram: '',
+            line: '',
+            gitlab: '',
+            weibo: '',
+            codepen: '',
+            youtube: '',
+            soundcloud: ''
+        }
+    }
 };
--- content/posts/2020-04-13-hello-world.md
+++ content/posts/2020-04-13-hello-world.md
@@ -5,13 +5,14 @@ slug: hello-world
 draft: false
 date: 2020-03-04T13:05:19.103Z
 description: >-
-  Hey, this is my first blog, describing a little bit about me, why I started
-  this blog.
+    Hey, this is my first blog, describing a little bit about me, why I started
+    this blog.
 category:
-  - User
+    - User
 tags:
-  - 'Me :)'
+    - 'Me :)'
 ---
+
 Hey all,
 
 Thanks for coming on this blog :)
@@ -24,14 +25,12 @@ This one is a long due thing which I was thinking since the beginning of time. T
 
 I'm Nimish Gupta. A Software Engineer, currently working at an amazing company called Pesto. Here I have worked as a Software Engineer from last 3 years. In the company, I have worked on a couple of languages/frameworks/databases/environments. I like to automate stuff and program deliberately.
 
-I have experience in various trending technologies such as **React, Reasonml, Typescript, Serverless, Docker, Node and ...**. Feel free to check out my [Github](https://github.com/nimish-gupta),  [LinkedIn](https://www.linkedin.com/in/nimish-gupta) or [my portfolio](https://portfolio.nimish-gupta.dev) for more examples of my work.
+I have experience in various trending technologies such as **React, Reasonml, Typescript, Serverless, Docker, Node and ...**. Feel free to check out my [Github](https://github.com/nimish-gupta), [LinkedIn](https://www.linkedin.com/in/nimish-gupta) for more examples of my work.
 
 The reason I have started this blog was to improve my writing skills (especially technical writing skills) and learn how to express programming and give back to the community. There is this Chinese saying,
 
 > If you want happiness for an hour, take a nap. If you want happiness for a day, go fishing. If you want happiness for a year, inherit a fortune. If you want happiness for a lifetime, help somebody.
 
 Also, [this](https://www.swyx.io/writing/learn-in-public/) blog from [@swyx](https://twitter.com/swyx) explains multiple benefits of writing in public which really inspired me to start this blog.
-
-
 
 Feel free to say hi [@twitter](https://twitter.com/nimishrocks2).
